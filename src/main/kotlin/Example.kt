4e1517728a1a350b2b7915b676cc9345d9b1b38f
Christoph Baudson <christoph.baudson@rewe-digital.com>
2018-05-13 21:00:45 +0200

    Some Typealias foo

--- src/main/kotlin/Example.kt
+++ src/main/kotlin/Example.kt
@@ -2,3 +2,16 @@ import arrow.core.Option
 
 val mightContainTheMeaningOfLife: Option<Int> = Option.invoke(42)
 
+
+typealias ToString = (Int) -> String
+
+val f: (Int) -> String = { "foo" + "o".repeat(it) }
+
+fun stringBuilder(toString: ToString, n:Int):String {
+    return toString(n)
+}
+
+fun main(args : Array<String>) {
+    val x:String = stringBuilder(f, 7)
+    println(x)
+}
